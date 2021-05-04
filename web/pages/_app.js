35a246694e555d34621d00e10a4ed7087a645c4e
MoonBarc <39464198+MoonBarc@users.noreply.github.com>
2021-05-04 13:42:30 -0400

    delete web project

--- web/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import Wrapper from '../comps/wrapper'
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Wrapper><Component {...pageProps} /></Wrapper>
-}
-
-export default MyApp
